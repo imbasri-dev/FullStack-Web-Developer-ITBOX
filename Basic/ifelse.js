1e1cdd01e5ea80e43058b2ac5817b64917ed2fb9
imbasri.dev <imbasri.dev@gmail.com>
2022-08-22 20:48:36 +0700

    update learn

---
+++ Basic/ifelse.js
@@ -0,0 +1,8 @@
+//kondisi
+let umur = 17;
+
+if (umur >= 17) {
+  console.log("Boleh buat KTP");
+} else {
+  console.log("Tidak boleh buat KTP");
+}
---
+++ Basic/swith.js
@@ -0,0 +1,10 @@
+let minuman = "kopi";
+
+switch (minuman) {
+  case "kopi":
+    console.log("Pesen Kopi");
+    break;
+  case "teh":
+    console.log("Pesen Teh");
+    break;
+}
