0a6ee24a326147c7454a0c4694c7dc1b3564f524
imbasri-dev <imbasri.dev@gmail.com>
2022-08-31 19:14:33 +0700

    learn intermediate : desctructuringElement

---
+++ Intermediate/destrukturisasiElement.js
@@ -0,0 +1,24 @@
+// desktrukturing element = mengubah struktur element dengan cara yang lebih elegan
+
+// contoh :
+// desctructuring array
+let items = ["Asus", "laptop"];
+let [nama, type] = items;
+
+console.log(nama);
+console.log(type);
+
+let students = ["Joko", 3.55, true]; //data dalam array belom tahu isi apa , maka perlu diubah menggunakan desctructuring
+
+let [name, gpa, isGraduate] = students;
+
+console.log(name);
+console.log(gpa);
+console.log(isGraduate);
+
+// kenapa kita gunakan ES6
+/*
+1.sintaks lebih modern
+2.koding lebih singkat padat
+3.framework sekarang menggunakan ES6
+*/
