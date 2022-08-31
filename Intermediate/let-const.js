7a290dae11d36a77ce1b326b16bd9b0a6f28c781
imbasri-dev <imbasri.dev@gmail.com>
2022-08-31 16:14:50 +0700

    learn intermediate : let and const

---
+++ Intermediate/let-const.js
@@ -0,0 +1,15 @@
+/*
+let : dapat dipakai untuk menyiapkan variabel yang dapat diisi nilai berkali-kali,
+const : hanya sekali saja.
+
+let dan const tidak dapat dipanggil ulang
+
+*/
+
+let angka = 10;
+angka = 100;
+console.log(angka); //100
+
+const angkaBaru = 10;
+angkaBaru = 100;
+console.log(angkaBaru); // error
