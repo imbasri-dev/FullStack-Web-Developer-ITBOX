ae29b05f233ef2f64418900820edea4ed3c354dc
imbasri-dev <imbasri.dev@gmail.com>
2022-09-01 09:16:50 +0700

    learn intermediate : OOP Konsep

--- Intermediate/OOPKonsep.js
+++ Intermediate/OOPKonsep.js
@@ -39,3 +39,23 @@ class Student {
     this._gpa = gpa;
   }
 }
+
+// example
+let user = {
+  name: "John",
+  surname: "Smith",
+
+  get fullName() {
+    return `${this.name} ${this.surname}`;
+  },
+
+  set fullName(value) {
+    [this.name, this.surname] = value.split(" ");
+  },
+};
+
+// variabel fullName dijalankan dengan value ditetapkan.
+user.fullName = "Alice Cooper";
+
+console.log(user.name); // Alice
+console.log(user.surname); // Cooper
