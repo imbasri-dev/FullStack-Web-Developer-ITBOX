3de7922145191775ce6ad4a43df4bb5a1265eeed
imbasri <imbasri.dev@gmail.com>
2023-05-20 22:14:14 +0700

    update todo : put and delete

--- Advanced/backend_expressSquelize/controllers/TodoController.js
+++ Advanced/backend_expressSquelize/controllers/TodoController.js
@@ -44,5 +44,46 @@ class TodoController {
         res.json(err);
       });
   }
+  static deleteTodo(req,res){
+    let id = +req.params.id
+
+    todo.destroy({
+      where:{
+        id
+      }
+    }).then(result=>{
+      if(result === 1 ){
+       return res.json({
+          message:`Todo has been Delete ${id}`
+        })
+      }
+      res.status(400).json({
+        message:'Todo failed to Delete!'
+      })
+    }).catch(err=>{
+      res.json(err)
+    })
+  }
+   static updateTodo(req,res){
+    let id = +req.params.id
+    const {task,status} = req.body
+
+    todo.update({task,status: Boolean(status)},{
+      where:{
+        id
+      },
+      returning:true,
+      plain:true
+    }).then(result=>{
+      console.log(result[1])
+        return res.status(201).json({
+          message:"data has been update",
+         data:result[1]
+        })
+      
+    }).catch(err=>{
+      res.status(500).json(err)
+    })
+  }
 }
 module.exports = TodoController;
--- Advanced/backend_expressSquelize/routes/todo.js
+++ Advanced/backend_expressSquelize/routes/todo.js
@@ -5,6 +5,8 @@ const TodoController = require('../controllers/TodoController')
 todoRoute.get('/',TodoController.getTodos)
 todoRoute.post('/add',TodoController.addTodos)
 todoRoute.get('/find/:id',TodoController.findById)
+todoRoute.delete('/delete/:id',TodoController.deleteTodo)
+todoRoute.put('/update/:id',TodoController.updateTodo)
 
 module.exports = todoRoute
 
