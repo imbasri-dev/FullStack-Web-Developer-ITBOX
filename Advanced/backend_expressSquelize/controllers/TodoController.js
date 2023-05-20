92723c07601de79e92beb9f5573dce8b456fe5b8
imbasri <imbasri.dev@gmail.com>
2023-05-20 14:26:13 +0700

    add todos , squelize

--- Advanced/backend_expressSquelize/controllers/TodoController.js
+++ Advanced/backend_expressSquelize/controllers/TodoController.js
@@ -1,9 +1,17 @@
 class TodoController {
     static getTodos(req,res){
-        res.send('Page Todos Utama')
+        const arrObj = [{
+            id:1,
+            name:'laptop'
+        },{
+            id:2,name:'komputer'
+        }]
+        res.json(arrObj)
     }
     static addTodos(req,res){
-        res.send('Page Add')
+        res.json({
+            message:"Page add Todos"
+        })
     }
 }
 module.exports = TodoController
